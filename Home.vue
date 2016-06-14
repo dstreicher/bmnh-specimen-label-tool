135cc9954ea8bfba00e7bd5162fb04d685b298d2
Daniel Streicher <dstreicher@bitbucket.org>
2016-06-13 23:56:07 -0400

    updated readme

---
+++ Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <div class="col-xs-4">.col-xs-4</div>
+    <div class="col-xs-4">.col-xs-4</div>
+    <div class="col-xs-4">.col-xs-4</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        quote: 'foo'
+      }
+    },
+    methods: {
+      getQuote() {
+        this.quote = 'bar';
+      }
+    }
+  }
+</script>
+
+<style>
+  [class*="col-"] {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    background-color: rgba(86, 61, 124, .15);
+    border: 1px solid rgba(86, 61, 124, .2);
+  }
+</style>
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # bmnh-specimen-label-tool
+
+> A web application for generating and updating specimen collection labels
+
+## Prerequisites
+
+* Oracle VM VirtualBox (https://www.virtualbox.org/)
+* Vagrant (https://www.vagrantup.com/)
+
+## Build Setup
+
+``` bash
+# provision environment
+vagrant up
+
+# launch bash shell
+vagrant ssh
+cd /vagrant
+
+# install dependencies
+npm install
+
+# start server
+npm start
+```
+
+## Development
+
+``` bash
+# serve with hot reload at localhost:8080
+npm run dev
+```
