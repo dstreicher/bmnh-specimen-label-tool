e2e13bd7764ea6555f771b0f34b51fef2df1cc02
Daniel Streicher <dstreicher@bitbucket.org>
2016-06-17 01:01:08 -0400

    updated export labels page and removed duplicate data

--- app/pages/ExportLabels.vue
+++ app/pages/ExportLabels.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="row">
-    <div class="col-xs-12">Export Labels</div>
+  <div>
+    <div class="card" v-for="specimen in specimens">
+      <h3 class="card-header">{{specimen.catalogNumber}}</h3>
+      <div class="card-block">
+        <h4 class="card-title">{{specimen.genus + ' ' + specimen.species}}</h4>
+        <!--<p class="card-text">{{specimen.}}</p>-->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        specimens: {}
+      }
+    },
+    created() {
+      var view = this;
+      var specimen = this.$resource('api/specimens{/id}');
+      specimen.query().then((res) => {
+        this.specimens = res.data;
+      }, (res) => {
+        console.log('failure!');
+      });
+    }
   }
 </script>
 
