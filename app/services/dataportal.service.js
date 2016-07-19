8d1a2272de31b5c92d9efcfe3f86bb52f451b4be
Daniel Streicher <dstreicher@bitbucket.org>
2016-07-19 02:30:46 -0400

    added dataportal service

---
+++ app/services/dataportal.service.js
@@ -0,0 +1,13 @@
+import cvars from './cvars.service'
+
+export default {
+  store: {},
+  search(context, catalogNumber) {
+    var queryString = JSON.stringify({
+      catalogNumber: catalogNumber,
+      subDepartment: cvars.EMU_SPECIMENS_SUB_DEPARTMENT_REPTILES_AMPHIBIANS
+    });
+    var query = cvars.RESOURCE_URL + '&filters=' + encodeURIComponent(queryString);
+    return context.$http.get(query);
+  }
+}
